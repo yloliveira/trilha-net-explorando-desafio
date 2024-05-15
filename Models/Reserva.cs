18d52bfb4eaa4f020782605ef64d38f8d38a905f
Yan Oliveira <oliveira.yan02@gmail.com>
2024-05-15 16:55:52 -0300

    feat: should be able to get the quantity of guests

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -35,9 +35,7 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            return Hospedes.Count;
         }
 
         public decimal CalcularValorDiaria()
